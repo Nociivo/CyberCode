bc70054edfd316baf4a8c336ec07c96315ed9ea7
Nociivo <71148474+Nociivo@users.noreply.github.com>
2021-11-20 13:23:55 -0300

    Delete ds directory

--- ds/php/bioeredesdelete.php
+++
@@ -1,7 +0,0 @@
-<?php
-    include_once "conexao.php";
-    $idBio = $_GET['idBio'];
-
-    $del = mysqli_query($conn,"DELETE FROM tbbioredes WHERE idBio = '$idBio'");
-     
-?>
--- ds/php/profileid.php
+++
@@ -1,5 +0,0 @@
-<?php
-include_once('conexao.php');
-    $id = $_GET['codAluno'];
-    //... look up username from database ...
-    header("Location: ../profile.php" . $id);
